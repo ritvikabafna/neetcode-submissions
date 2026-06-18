9916a778ff2372a93a90fbbcaaf021e4719b8903
Ritvika Bafna <bafnaritvika@gmail.com>
2026-06-19 00:27:31 +0530

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        vector<int> ans(temp.size(),0);
+        stack<int> st;
+        for(int i=0;i<temp.size();i++){
+            while(!st.empty() && temp[i]>temp[st.top()]){
+                int idx=st.top();
+                st.pop();
+                ans[idx]=i-idx;
+            }
+            st.push(i);
+        }
+        return ans;
+          
+    }
+};
